9dafb50989b3bc3014dac28e0884dbc2ee2187fc
solveigsg12 <solveigsif91@gmail.com>
2022-05-05 23:17:57 +0200

    add prettier and prettify files

--- omdb-web/pages/index.js
+++ omdb-web/pages/index.js
@@ -1,24 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import styles from '../styles/Home.module.css'
+import React, { useEffect, useState } from "react";
+
+import styles from "../styles/Home.module.css";
+
+import Button from "../components/Button";
 
 export default function Home() {
   const [movieTitle, setMovieTitle] = useState("lucky+number+slevin");
-  const [movieInfo, setMovieInfo] = useState([])
-  useEffect(()=> {
+  const [movieInfo, setMovieInfo] = useState([]);
+  useEffect(() => {
     fetch(`http://www.omdbapi.com/?apikey=aa7c9259&t=+${movieTitle}`)
-    .then(res => res.json())
-    .then(
-      (data) => {
-        setMovieInfo(data); 
-      },
-      (error) => {
-        console.log(error)
-      }
-    )
-  },[movieTitle])
+      .then((res) => res.json())
+      .then(
+        (data) => {
+          setMovieInfo(data);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }, [movieTitle]);
   return (
     <div className={styles.container}>
-    helloo
+      <Button>submit</Button>
     </div>
-  )
+  );
 }
