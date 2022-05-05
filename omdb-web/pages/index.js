c325c3806664b6b7991385e97d2f73b825581ce4
solveigsg12 <solveigsif91@gmail.com>
2022-05-05 21:38:35 +0200

    adding material ui icons and saving data to state

--- omdb-web/pages/index.js
+++ omdb-web/pages/index.js
@@ -1,23 +1,21 @@
 import React, { useEffect, useState } from 'react';
-
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const [movieTitle, setMovieTitle] = useState("lucky+number+slevin");
-useEffect(()=> {
-  fetch(`http://www.omdbapi.com/?apikey=aa7c9259&t=+${movieTitle}`)
-  .then(res => res.json())
-  .then(
-    (data) => {
-      console.log(data)
-    },
-    (error) => {
-      console.log(error)
-    }
-  )
-},[movieTitle])
+  const [movieInfo, setMovieInfo] = useState([])
+  useEffect(()=> {
+    fetch(`http://www.omdbapi.com/?apikey=aa7c9259&t=+${movieTitle}`)
+    .then(res => res.json())
+    .then(
+      (data) => {
+        setMovieInfo(data); 
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  },[movieTitle])
   return (
     <div className={styles.container}>
     helloo
